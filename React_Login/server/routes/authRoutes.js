4ce42c379cb27456b6abb439e77be22fda1dc116
Sefinek <sefinek5916@gmail.com>
2023-08-07 18:50:38 +0200

    Delete useless middleware

--- React_Login/server/routes/authRoutes.js
+++ React_Login/server/routes/authRoutes.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
 const { test, registerUser, loginUser, getProfile } = require('../controllers/authController');
 
 // Middleware
-router.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:5173', // Allow requests from your frontend's origin
-  })
-);
-
-
-
 
 router.get('/', test);
 router.post('/register', registerUser);
