69e9d60c5b2342466b8692a561638fdd81d89fa4
Sefinek <sefinek5916@gmail.com>
2023-08-07 18:50:15 +0200

    Fix endpoint /dashboard

--- React_Login/client/src/pages/Dashboard.jsx
+++ React_Login/client/src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
-import {useContext} from 'react'
-import { UserContext } from '../../context/userContext'
+import React, { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
 
 export default function Dashboard() {
-    const {user} = useContext(useContext)
-  return (
-    <div>
-       <h1>Dashboard</h1>
-       {!!user && (<h1>Witaj {user.name}! </h1> )}
-    </div>
-  )
+    const { user } = useContext(UserContext);
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {!!user && <h1>Witaj {user.name}! </h1>}
+        </div>
+    );
 }
